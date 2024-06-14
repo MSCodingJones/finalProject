2ac5ea8e48d4ce84e9f4ab5cad5acf406d503e7c
MSCodingJones <sm.jones424@icloud.com>
2024-06-14 10:26:34 -0500

    create components

---
+++ react/src/components/Footer.js
@@ -0,0 +1,14 @@
+const Footer =()=> {
+
+    return (
+        <footer className="footer" id="footer">
+            <div className="container">
+                <p className="colophon text-center"> site by
+                    <a className="site-link" href="wwww.ellahoney.com" target="_blank">shaquella</a>
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- react/src/index.js
+++ react/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+import App from './App'
 
 const root = ReactDom.createRoot(document.getElementById ('root'))
 
-root.render(<h1>Ella Honey</h1>)
+console.log(BrowserRouter)
+
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)

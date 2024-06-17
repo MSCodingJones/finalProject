f99eac0915d6b558d01f81c025c5214bbdfdb490
MSCodingJones <sm.jones424@icloud.com>
2024-06-17 11:09:10 -0500

    update react

--- react/src/App.js
+++ react/src/App.js
@@ -1,11 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import ErrorPage from "./components/ErrorPage"
+import Home from "./components/Home"
+import About from "./components/About"
+import Store from "./components/Store"
 
 const App=()=> {
 
     return (
         <>
             <Header />
+            <Routes>
+                <Route path='/' element={ <Home />} />
+                <Route path='/about' element={ <About />} />
+                <Route path='/Store' element={ <Store />} />
+            </Routes>
             <Footer />
         </>
     )
